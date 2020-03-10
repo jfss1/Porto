b5e2f3d61574f907eb1f80dec2055c7357eba73b
jfss1 <jose.filipe.sampaio@live.com.pt>
2020-03-10 21:26:38 +0000

    Exercicio 3 Resolvido

--- Porto/src/Porto/Porto.java
+++ Porto/src/Porto/Porto.java
@@ -71,7 +71,22 @@ public class Porto {
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Porto porto = new Porto(3);
+        Petroleiro petro1 = new Petroleiro("1234");
+        petro1.setCarga(200);
+        PortaContentores pConta1 = new PortaContentores("2345");
+        pConta1.setNoContentores(50);
+        PortaContentores pConta2 = new PortaContentores("3456");
+        pConta2.setNoContentores(20);
+        Navio n = new Navio("0123");
+        Petroleiro petro2 = new Petroleiro("1234");
+        petro2.setCarga(200);
+        porto.novoNavio(petro1);
+        porto.novoNavio(petro2);
+        porto.novoNavio(pConta1);
+        porto.novoNavio(pConta2);
+        porto.novoNavio(n);
+        System.out.println("Capacidade total = " + porto.getCapacidadeTotal() + " toneladas.");
     }
 
 }
