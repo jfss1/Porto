65b05461ab495fd24ece84f590cb1e74004a4696
jfss1 <jose.filipe.sampaio@live.com.pt>
2020-03-10 21:25:03 +0000

    Exercicio2 Resolvido
    Fez-se o exercicio 2

--- Porto/src/Porto/Porto.java
+++ Porto/src/Porto/Porto.java
@@ -11,11 +11,67 @@ package Porto;
  */
 public class Porto {
 
+    private int ctNavios;
+    private int noMaxNavios;
+    private Navio[] navios;
+
+    public Porto(int noMaxNavios) {
+        this.noMaxNavios = noMaxNavios;
+        navios = new Navio[noMaxNavios];
+        ctNavios = 0;
+    }
+
     /**
-     * @param args the command line arguments
+     * Get the value of navios
+     *
+     * @return the value of navios
      */
+    public Navio[] getNavios() {
+        return navios;
+    }
+
+    /**
+     * Get the value of noMaxNavios
+     *
+     * @return the value of noMaxNavios
+     */
+    public int getNoMaxNavios() {
+        return noMaxNavios;
+    }
+
+    public void novoNavio(Navio n) {
+        if (ctNavios == noMaxNavios) {
+            System.out.println("No máximo de navios (" + noMaxNavios + ") atingido!");
+            return;
+        }
+        boolean enc = false;
+        for (int i = 0; i < ctNavios; i++) {
+            if (n.getMatricula().equals(navios[i].getMatricula())) {
+                enc = true;
+                System.out.println("Matrcula existente: " + n.getMatricula());
+            }
+        }
+        if (!enc) {
+            navios[ctNavios] = n;
+            ctNavios++;
+        }
+    }
+
+    public float getCapacidadeTotal() {
+        float total = 0;
+        for (int i = 0; i < ctNavios; i++) {
+            if (navios[i] instanceof Petroleiro) {
+                total += ((Petroleiro) navios[i]).getCarga();
+            }
+            if (navios[i] instanceof PortaContentores) {
+                total += ((PortaContentores) navios[i]).getNoContentores() * 10;
+            }
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
